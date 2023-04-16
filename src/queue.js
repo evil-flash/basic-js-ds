fb8f69e81bfb22215af88ec924fa35c66f2a7566
evil_FlasH <evil.flash@yandex.ru>
2023-04-16 21:03:23 +0300

    feat: example tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+/* const { NotImplementedError } = require('../extensions/index.js'); */
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,19 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let list = new ListNode(null);
+    if (this.length > 0){
+      list = this.head
+    };
+    return list;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const headNode = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return headNode.value;
   }
 }
 
